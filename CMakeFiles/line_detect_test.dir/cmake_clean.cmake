a3c028bceae02fd40dd5757b1c45c4e80320ce60
Mok <mokok118@kakao.com>
2023-10-29 20:34:56 +0900

    init_cmake

---
+++ CMakeFiles/line_detect_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/line_detect_test.dir/src/opencv_test.cpp.o"
+  "line_detect_test.pdb"
+  "line_detect_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/line_detect_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
