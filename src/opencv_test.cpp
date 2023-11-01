586ec725ddbf4d433a04dd7839b44e9c0ec04518
Mok <mokok118@kakao.com>
2023-11-02 07:13:00 +0900

    .cache/

--- src/opencv_test.cpp
+++ src/opencv_test.cpp
@@ -2,6 +2,7 @@
 // Authors: JinWoo Mok
 
 #include <iostream>
+#include <opencv2/imgproc.hpp>
 #include <vector>
 #include "opencv2/opencv.hpp"
 
@@ -17,7 +18,7 @@ void on_mouse(int event, int x, int y, [[maybe_unused]]int flags, void*){
 }
 
 int main(){
-    cv::VideoCapture cap("../resource/project_video.mp4");
+    cv::VideoCapture cap("../resource/Sub_project.avi");
 
     if(!cap.isOpened()){
         std::cerr << "Video open failed!" << std::endl;
@@ -26,8 +27,8 @@ int main(){
 
     double fps = cap.get(cv::CAP_PROP_FPS);
     int delay = cvRound(1000 / fps);
-    
-    cv::Mat frame;
+    [[maybe_unused]]int h[256] = {};
+    cv::Mat frame, gray_scale_img, gaussian_img, canny_img;
 
     while(true){
         cap >> frame;
@@ -36,7 +37,16 @@ int main(){
         }
     
         cv::setMouseCallback("frame", on_mouse);
+
+        cv::cvtColor(frame, gray_scale_img, cv::COLOR_BGR2GRAY);
+        cv::GaussianBlur(gray_scale_img, gaussian_img, cv::Size(), 2.0);
+        cv::Canny(gray_scale_img, canny_img, 50, 150);
+
         imshow("frame", frame);
+        imshow("Gray_img", gray_scale_img);
+        imshow("Gaussian_img", gaussian_img);
+
+        //imshow("Canny_img", canny_img);
 
         // ESC is exit key
         if(cv::waitKey(delay) == 27){
