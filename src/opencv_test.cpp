f28fb928de026262e7d86acd22eac769dbc1d418
Mok <mokok118@kakao.com>
2023-11-03 11:15:40 +0900

    update_threshold

--- src/opencv_test.cpp
+++ src/opencv_test.cpp
@@ -2,14 +2,14 @@
 // Authors: JinWoo Mok
 
 #include <iostream>
-#include <opencv2/core.hpp>
-#include <opencv2/videoio.hpp>
 #include <vector>
 
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
 #include "opencv2/opencv.hpp"
 
 cv::Mat RegionOfInterest(cv::Mat canny_img){
-    const int x_ = 250;
+    const int x_ = 255;
     const cv::Point p1_(0, 390), p2_(x_, 410);
     const cv::Point p3_(640 - x_, 390), p4_(640, 410);
     const cv::Rect rc1(p1_, p2_);
@@ -24,8 +24,7 @@ cv::Mat RegionOfInterest(cv::Mat canny_img){
 	cv::Mat img_masked;
 	bitwise_and(canny_img, img_mask, img_masked);
 
-    imshow("frame", img_mask);
-    imshow("Gray_img", img_masked);
+    imshow("Mask_img", img_mask);
 
     return img_masked;
 }
@@ -38,21 +37,28 @@ int main(){
         return -1;
     }
 
-    double fps = cap.get(cv::CAP_PROP_FPS);
+    const double fps = cap.get(cv::CAP_PROP_FPS);
     const int delay = cvRound(1000 / fps);
     const int width = cvRound(cap.get(cv::CAP_PROP_FRAME_WIDTH));
-    //const int height = cvRound(cap.get(cv::CAP_PROP_FRAME_HEIGHT));
-    const cv::Point p1(0, 400), p2(width, 400);
+    const int y_offset = 400;
+    const cv::Point p1(0, y_offset), p2(width, y_offset);
 
     cv::Mat frame, dst, gray_scale_img, gaussian_img, canny_img, img_edge, hough_img;
     
-    std::vector<cv::Vec4i> lines(4);
+    std::vector<cv::Vec4i> lines;
+    std::vector<int> v_l_pos;
+    std::vector<int> v_r_pos;
     
     float hough_rho = 2;
     float hough_theta = CV_PI/180;
     float hough_threshold = 15;
     float min_line_length = 10;
     float max_line_length = 20;
+
+    int prev_l_pos = 0;
+    int prev_r_pos = 0;
+    int cur_l_pos = 0;
+    int cur_r_pos = 0;
 
     while(true){
         cap >> frame;
@@ -61,19 +67,66 @@ int main(){
         }
     
         cv::cvtColor(frame, gray_scale_img, cv::COLOR_BGR2GRAY);
-        cv::GaussianBlur(gray_scale_img, gaussian_img, cv::Size(), 3.0);
-        cv::Canny(gray_scale_img, canny_img, 240, 250);
+        cv::GaussianBlur(gray_scale_img, gaussian_img, cv::Size(), 1.0);
+        cv::Canny(gaussian_img, canny_img, 240, 250);
         img_edge = RegionOfInterest(canny_img);
         img_edge.copyTo(hough_img);
         cv::HoughLinesP(hough_img, lines, hough_rho, hough_theta, hough_threshold, min_line_length, max_line_length);
-
+        
         dst = frame.clone();
-        cv::line(dst, p1, p2, cv::Scalar(0, 255, 0), 1, cv::LINE_AA);
+        cv::line(dst, p1, p2, cv::Scalar(255, 0, 221), 2, cv::LINE_AA);
+        cv::circle(dst, cv::Point(width / 2, y_offset), 10, cv::Scalar(255, 0, 221), 2, cv::LINE_AA);
+
         for(cv::Vec4i line : lines){
-            cv::line(dst, cv::Point(line[0], line[1]), cv::Point(line[2], line[3]), cv::Scalar(255, 0, 0), 2, cv::LINE_AA);
-            // std::cout << line[0] << " " << line[1];
-            // std::cout << line[2] << " " << line[3] << std::endl;
+            if(line[2] < width / 2 && line[0] < width / 2){
+                if(v_l_pos.size() != 10){
+                    v_l_pos.push_back(line[2]);
+                    v_l_pos.push_back(line[0]);
+                }
+                else{
+                    int sum = 0;
+                    for(int num : v_l_pos){
+                        sum += num;
+                    }
+                    cur_l_pos = sum / 10;
+
+                    if(prev_l_pos == 0){
+                        prev_l_pos = cur_l_pos;
+                    }
+
+                    if(abs(prev_l_pos - cur_l_pos) < 70){
+                        prev_l_pos = cur_l_pos;
+                    }
+                    v_l_pos.clear();
+                }
+            }
+            else if(line[2] > width / 2 && line[0] > width / 2){
+                if(v_r_pos.size() != 10){
+                    v_r_pos.push_back(line[2]);
+                    v_r_pos.push_back(line[0]);
+                }
+                else{
+                    int sum = 0;
+                    for(int num : v_r_pos){
+                        sum += num;
+                    }
+                    cur_r_pos = sum / 10;
+
+                    if(prev_r_pos == 0){
+                        prev_r_pos = cur_r_pos;
+                    }
+
+                    if(abs(prev_r_pos - cur_r_pos) < 70){
+                        prev_r_pos = cur_r_pos;
+                    }
+                    v_r_pos.clear();
+                }
+            }
         }
+
+        cv::circle(dst, cv::Point(prev_l_pos, y_offset), 5, cv::Scalar(0, 255, 0), 2, cv::LINE_AA);
+        cv::circle(dst, cv::Point(prev_r_pos, y_offset), 5, cv::Scalar(0, 255, 0), 2, cv::LINE_AA);
+        cv::circle(dst, cv::Point((prev_l_pos + prev_r_pos) / 2, y_offset), 5, cv::Scalar(0, 0, 255), 2, cv::LINE_AA);
 
         imshow("frame", frame);
         imshow("Gray_img", gray_scale_img);
@@ -81,7 +134,6 @@ int main(){
         imshow("Canny_img", canny_img);
         imshow("Roi_img", img_edge);
         imshow("Dst_img", dst);
-
 
         // ESC is exit key
         if(cv::waitKey(delay) == 27){
